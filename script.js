13dbfe6169c32722e65c3c8aa866da3e65bd579d
Andjela Vucic <andjela.vucic@codemine.me>
2024-02-19 17:21:19 +0100

    COD-103: feat: add event listeners for thumbnail clicks, modal display, and mobile menu toggle

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
-const mainImg = document.querySelector(".main-img");
+const mainImg = document.querySelector('.main-img');
+const thumbs = document.querySelectorAll('.thumbnails img');
 
-const thumb1 = document.querySelector("#thumb1");
-const thumb2 = document.querySelector("#thumb2");
-const thumb3 = document.querySelector("#thumb3");
-const thumb4 = document.querySelector("#thumb4");
-
-thumb1.addEventListener("click", () => {
-    mainImg.src = "./images/image-product-1.jpg";
+thumbs.forEach(thumb => {
+    thumb.addEventListener('click', function () {
+        mainImg.src = this.src;
+    });
 });
 
-thumb2.addEventListener("click", () => {
-    mainImg.src = "./images/image-product-2.jpg";
+const modal = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal-overlay');
+const gallery = document.querySelector('.image-gallery');
+
+mainImg.addEventListener('click', () => {
+    const galleryCopy = gallery.cloneNode(true);
+
+    const mainImgModal = galleryCopy.querySelector('.main-image');
+
+    const thumbnailImages = galleryCopy.querySelectorAll('.thumbnails img');
+
+    thumbnailImages.forEach((thumbnail) => {
+        thumbnail.addEventListener('click', function () {
+            mainImgModal.src = this.src;
+        });
+    });
+    modal.appendChild(galleryCopy);
+
+    modal.style.display = 'block';
+    modalOverlay.style.display = 'block';
 });
 
-thumb3.addEventListener("click", () => {
-    mainImg.src = "./images/image-product-3.jpg";
+const mobileMenu = document.querySelector('.mobile-menu-overlay');
+const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+
+mobileMenuIcon.addEventListener('click', () => {
+    modalOverlay.style.display = 'block';
+    mobileMenu.style.display = 'block';
+
+    const navElems = document.querySelector('.nav-items');
+
+    const clonedNavElems = navElems.cloneNode(true);
+    mobileMenu.appendChild(clonedNavElems);
+    clonedNavElems.style.display = 'block';
+
+    const iconClose = document.querySelector('.icon-close');
+    iconClose.addEventListener('click', () => {
+        modalOverlay.style.display = 'none';
+        mobileMenu.style.display = 'none';
+
+        clonedNavElems.remove();
+    });
 });
-
-thumb4.addEventListener("click", () => {
-    mainImg.src = "./images/image-product-4.jpg";
-});
-
-
-const modal = document.querySelector(".modal");
-const modalOverlay = document.querySelector(".modal-overlay");
-const gallery = document.querySelector(".gallery");
-
-mainImg.addEventListener("click", () => {
-  const galleryCopy = gallery.cloneNode(true);
-  
-  modal.appendChild(galleryCopy);
-
-  modal.style.display = "block";
-  modalOverlay.style.display = "block";
-});
-
